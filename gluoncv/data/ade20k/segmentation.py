d47a3f90fd7d59be4d3c1fb45be497de324e1805
whughw <ghwdsz@163.com>
2020-08-02 00:07:40 +0800

    Update segmentation.py

--- gluoncv/data/ade20k/segmentation.py
+++ gluoncv/data/ade20k/segmentation.py
@@ -33,70 +33,10 @@ class ADE20KSegmentation(SegmentationDataset):
     >>>     num_workers=4)
     """
     # pylint: disable=abstract-method
-    BASE_DIR = 'ADEChallengeData2016'
-    NUM_CLASS = 150
-    CLASSES = ("wall", "building, edifice", "sky", "floor, flooring", "tree",
-               "ceiling", "road, route", "bed", "windowpane, window", "grass",
-               "cabinet", "sidewalk, pavement",
-               "person, individual, someone, somebody, mortal, soul",
-               "earth, ground", "door, double door", "table", "mountain, mount",
-               "plant, flora, plant life", "curtain, drape, drapery, mantle, pall",
-               "chair", "car, auto, automobile, machine, motorcar",
-               "water", "painting, picture", "sofa, couch, lounge", "shelf",
-               "house", "sea", "mirror", "rug, carpet, carpeting", "field", "armchair",
-               "seat", "fence, fencing", "desk", "rock, stone", "wardrobe, closet, press",
-               "lamp", "bathtub, bathing tub, bath, tub", "railing, rail", "cushion",
-               "base, pedestal, stand", "box", "column, pillar", "signboard, sign",
-               "chest of drawers, chest, bureau, dresser", "counter", "sand", "sink",
-               "skyscraper", "fireplace, hearth, open fireplace", "refrigerator, icebox",
-               "grandstand, covered stand", "path", "stairs, steps", "runway",
-               "case, display case, showcase, vitrine",
-               "pool table, billiard table, snooker table", "pillow",
-               "screen door, screen", "stairway, staircase", "river", "bridge, span",
-               "bookcase", "blind, screen", "coffee table, cocktail table",
-               "toilet, can, commode, crapper, pot, potty, stool, throne",
-               "flower", "book", "hill", "bench", "countertop",
-               "stove, kitchen stove, range, kitchen range, cooking stove",
-               "palm, palm tree", "kitchen island",
-               "computer, computing machine, computing device, data processor, "
-               "electronic computer, information processing system",
-               "swivel chair", "boat", "bar", "arcade machine",
-               "hovel, hut, hutch, shack, shanty",
-               "bus, autobus, coach, charabanc, double-decker, jitney, motorbus, "
-               "motorcoach, omnibus, passenger vehicle",
-               "towel", "light, light source", "truck, motortruck", "tower",
-               "chandelier, pendant, pendent", "awning, sunshade, sunblind",
-               "streetlight, street lamp", "booth, cubicle, stall, kiosk",
-               "television receiver, television, television set, tv, tv set, idiot "
-               "box, boob tube, telly, goggle box",
-               "airplane, aeroplane, plane", "dirt track",
-               "apparel, wearing apparel, dress, clothes",
-               "pole", "land, ground, soil",
-               "bannister, banister, balustrade, balusters, handrail",
-               "escalator, moving staircase, moving stairway",
-               "ottoman, pouf, pouffe, puff, hassock",
-               "bottle", "buffet, counter, sideboard",
-               "poster, posting, placard, notice, bill, card",
-               "stage", "van", "ship", "fountain",
-               "conveyer belt, conveyor belt, conveyer, conveyor, transporter",
-               "canopy", "washer, automatic washer, washing machine",
-               "plaything, toy", "swimming pool, swimming bath, natatorium",
-               "stool", "barrel, cask", "basket, handbasket", "waterfall, falls",
-               "tent, collapsible shelter", "bag", "minibike, motorbike", "cradle",
-               "oven", "ball", "food, solid food", "step, stair", "tank, storage tank",
-               "trade name, brand name, brand, marque", "microwave, microwave oven",
-               "pot, flowerpot", "animal, animate being, beast, brute, creature, fauna",
-               "bicycle, bike, wheel, cycle", "lake",
-               "dishwasher, dish washer, dishwashing machine",
-               "screen, silver screen, projection screen",
-               "blanket, cover", "sculpture", "hood, exhaust hood", "sconce", "vase",
-               "traffic light, traffic signal, stoplight", "tray",
-               "ashcan, trash can, garbage can, wastebin, ash bin, ash-bin, ashbin, "
-               "dustbin, trash barrel, trash bin",
-               "fan", "pier, wharf, wharfage, dock", "crt screen",
-               "plate", "monitor, monitoring device", "bulletin board, notice board",
-               "shower", "radiator", "glass, drinking glass", "clock", "flag")
-    def __init__(self, root=os.path.expanduser('~/.mxnet/datasets/ade'),
+    BASE_DIR = 'segmentation'
+    NUM_CLASS = 9
+    CLASSES = ("A","B","C","D","E","F","G","H","I")
+    def __init__(self, root=os.path.expanduser('~/.mxnet/datasets/Gaofen2020'),
                  split='train', mode=None, transform=None, **kwargs):
         super(ADE20KSegmentation, self).__init__(root, split, mode, transform, **kwargs)
         root = os.path.join(root, self.BASE_DIR)
@@ -155,9 +95,9 @@ def _get_ade20k_pairs(folder, mode='train'):
         mask_folder = os.path.join(folder, 'annotations/validation')
     for filename in os.listdir(img_folder):
         basename, _ = os.path.splitext(filename)
-        if filename.endswith(".jpg"):
+        if filename.endswith(".tif"):
             imgpath = os.path.join(img_folder, filename)
-            maskname = basename + '.png'
+            maskname = basename + '_gt.png'
             maskpath = os.path.join(mask_folder, maskname)
             if os.path.isfile(maskpath):
                 img_paths.append(imgpath)
